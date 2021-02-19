b0059d45cb0fb19e556e81840fc967bde89a59b0
jiae <vitaminpark7@gmail.com>
2021-02-19 14:53:41 +0900

    update header join btn

--- triplus/src/main/java/com/project/mega/triplus/form/JoinForm.java
+++ triplus/src/main/java/com/project/mega/triplus/form/JoinForm.java
@@ -1,15 +1,21 @@
 package com.project.mega.triplus.form;
 
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 import org.hibernate.validator.constraints.Length;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import java.io.Serializable;
 
 @Data
-public class JoinForm {
+@Getter @Setter
+public class JoinForm implements Serializable {
 
     @NotBlank
+    @Length(min = 1, max = 8, message = "닉네임을 1~8자 사이로 입력해주세요.")
     private String nickname;
 
     @NotBlank
@@ -18,6 +24,7 @@ public class JoinForm {
     private String email;
 
     @NotBlank
+    @Pattern(regexp="[a-zA-Z1-9]{8,12}", message = "비밀번호는 영어와 숫자로 포함해서 8~12자리 이내로 입력해주세요.")
     private String password;
 
     @NotBlank
