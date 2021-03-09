f9cebbd1090d55f4c0e2ea927dd3056620c9ef3d
dooyeong <noviced22@gmail.com>
2021-03-09 17:42:38 +0900

    .

--- triplus/src/main/java/com/project/mega/triplus/controller/UserController.java
+++ triplus/src/main/java/com/project/mega/triplus/controller/UserController.java
@@ -2,25 +2,14 @@ package com.project.mega.triplus.controller;
 
 
 import com.project.mega.triplus.entity.User;
-import com.project.mega.triplus.form.JoinForm;
 import com.project.mega.triplus.repository.UserRepository;
-import com.project.mega.triplus.service.CurrentUser;
 import com.project.mega.triplus.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
-import org.springframework.validation.Errors;
-import org.springframework.web.bind.annotation.*;
-
-import javax.annotation.PostConstruct;
-import javax.validation.Valid;
-import java.util.Collections;
-import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 @Slf4j
@@ -32,64 +21,6 @@ public class UserController {
     private final UserService userService;
 
     private final UserRepository userRepository;
-
-
-    @Transactional
-    @PostMapping("/join")
-    public String joinSubmit(
-            @RequestParam(value = "nickname") String nickname,
-            @RequestParam(value = "email") String email,
-            @RequestParam(value = "password") String password,
-            @RequestParam(value = "checklist") String checklist
-        ){
-        JoinForm joinForm = new JoinForm();
-
-        joinForm.setNickname(nickname);
-        joinForm.setEmail(email);
-        joinForm.setPassword(password);
-        joinForm.setAgreeTermsOfService(checklist);
-
-        User newUser = userService.processNewUser(joinForm);
-        userService.login(newUser);
-
-        return "redirect:/";
-    }
-
-//    @PostMapping("/checkNickName")
-//    @ResponseBody
-//    public String checkNickName(@RequestParam(value = "nickNameCheck")String nickName){
-//        String result=null;
-//
-//        if(userRepository.existsByNickName(nickName)){
-//            result="nickNameNO";
-//        } else{
-//            result="nickNameOK";
-//        }
-//
-//        return result;
-//    }
-//
-//    @Transactional
-//    @PostMapping("/join")
-//    @ResponseBody
-//    public String joinSubmit(
-//            @RequestParam(value = "nickname") String nickname,
-//            @RequestParam(value = "email") String email,
-//            @RequestParam(value = "password") String password,
-//            @RequestParam(value = "checklist") String checklist
-//        ){
-//        JoinForm joinForm = new JoinForm();
-//
-//        joinForm.setNickname(nickname);
-//        joinForm.setEmail(email);
-//        joinForm.setPassword(password);
-//        joinForm.setAgreeTermsOfService(checklist);
-//
-//        User newUser = userService.processNewUser(joinForm);
-//        userService.login(newUser);
-//
-//        return "joinSuccess";
-//    }
 
     @GetMapping("/check-email-token")
     @Transactional
